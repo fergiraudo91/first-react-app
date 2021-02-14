f3a0e07e2136c49cac252cab4033e5067bfa37b5
fergiraudo91 <fergiraudo91@gmail.com>
2021-02-14 09:10:55 -0300

    agregando docs

---
+++ docs/precache-manifest.892ea079a80459309c29727dc98a34e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6d3e121f7dfed93d5d2967c96f9da4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f43c7ba2b3daa2b9fec",
+    "url": "/static/css/main.df971f9a.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f43c7ba2b3daa2b9fec",
+    "url": "/static/js/main.bb48ef17.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
